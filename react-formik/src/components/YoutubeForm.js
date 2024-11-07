8146bc0a037a03d08093b9bd630352578ec182f6
Mownica_webdev <mownica.p@gmail.com>
2024-11-08 01:45:52 +0530

    validation

--- react-formik/src/components/YoutubeForm.js
+++ react-formik/src/components/YoutubeForm.js
@@ -13,7 +13,28 @@ function YoutubeForm() {
     onSubmit: (values) => {
       console.log("form values", values);
     },
+    validate: (values) => {
+      // values.name  values.email values.channel
+      // errors.name errors.email errors.channel
+      //errors.name = "This field is Required";
+      let errors = {};
+      if (!values.name) {
+        errors.name = "Required";
+      }
+      if (!values.email) {
+        errors.email = "Required";
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = "Invalid email format";
+      }
+      if (!values.channel) {
+        errors.channel = "Required";
+      }
+      return errors;
+    },
   });
+
   // console.log('"form values"', formik.values);
   return (
     <form onSubmit={formik.handleSubmit}>
