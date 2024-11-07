2cc3a8135363dff25e47ede44cad5de4315555f4
Mownica_webdev <mownica.p@gmail.com>
2024-11-08 05:17:06 +0530

    Form component replacing <form> tag

--- react-formik/src/components/YoutubeForm.js
+++ react-formik/src/components/YoutubeForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Formik } from "formik";
+import { Formik, Form } from "formik";
 import * as Yup from "yup";
 
 const initialValues = {
@@ -27,7 +27,7 @@ function YoutubeForm() {
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-      <form onSubmit={formik.handleSubmit}>
+      <Form>
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <input
@@ -61,7 +61,7 @@ function YoutubeForm() {
           ) : null}
         </div>
         <button type="submit">Submit</button>
-      </form>
+      </Form>
     </Formik>
   );
 }
