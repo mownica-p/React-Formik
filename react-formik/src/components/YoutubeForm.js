976303f3a3beaf56b1ed0967332bb485a59cbafb
Mownica_webdev <mownica.p@gmail.com>
2024-11-08 15:29:04 +0530

    ErrorMessage component  Revisited to customize its display

--- react-formik/src/components/YoutubeForm.js
+++ react-formik/src/components/YoutubeForm.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import TextError from "./TestError";
 
 const initialValues = {
   name: "",
@@ -33,13 +34,15 @@ function YoutubeForm() {
         <div className="form-control">
           <label htmlFor="name">Name</label>
           <Field type="text" id="name" name="name" />
-          <ErrorMessage name="name" />
+          <ErrorMessage name="name" component={TextError} />
         </div>
 
         <div className="form-control">
           <label htmlFor="email">Email</label>
           <Field type="email" id="email" name="email" />
-          <ErrorMessage name="email" />
+          <ErrorMessage name="email">
+            {(errorMsg) => <div className="error">{errorMsg}</div>}
+          </ErrorMessage>
         </div>
 
         <div className="form-control">
